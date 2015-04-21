8e94429a7f986a8803faf8a1fb116deea6f26bf9
SemihParlayan <semih.parlayan@hotmail.com>
2015-04-21 16:58:55 +0200

    Menu animations

--- TeamCrew/Assets/Scripts/UI/MainMenu.cs
+++ TeamCrew/Assets/Scripts/UI/MainMenu.cs
@@ -8,17 +8,21 @@ public class MainMenu : MonoBehaviour
     public Button singleplayerButton;
     public Button multiplayerButton;
 
+    private Animator anim;
+
     private int selectedButton = 1;
 
 	void Start () 
     {
         //ERROR CHECK RAWR
+        anim = GetComponent<Animator>();
 	}
 
 	void Update () 
     {
         //Select buttons
         float horizontal = Mathf.RoundToInt(Input.GetAxis("P1HL") + Input.GetAxis("P1HR"));
+
         if (horizontal > 0)
         {
             selectedButton = 2;
@@ -41,9 +45,17 @@ public class MainMenu : MonoBehaviour
             else if (selectedButton == 2)
             {
                 multiplayerButton.onClick.Invoke();
-                gameObject.SetActive(false);
+                //gameObject.SetActive(false);
             }
+
+            anim.SetTrigger("AnimationStart");
         }
 
 	}
+
+    public void SignAnimationComplete()
+    {
+        anim.SetTrigger("AnimationComplete");
+        gameObject.SetActive(false);
+    }
 }
