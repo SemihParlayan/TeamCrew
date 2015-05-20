bb052bfc094e302d2795c38e578c89eaec3a212a
SemihParlayan <semih.parlayan@hotmail.com>
2015-05-20 15:50:20 +0200

    Butterflies :bug: :wink:

---
+++ TeamCrew/Assets/Scripts/Spawning/ButterflySpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButterflySpawner : MonoBehaviour 
+{
+    public GameObject flyingButterflies;
+    public GameObject sittingButterflies;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "PlayerOne" || other.tag == "PlayerTwo")
+        {
+            ActivateButterFlies();
+        }
+    }
+
+    void ActivateButterFlies()
+    {
+        flyingButterflies.SetActive(true);
+        sittingButterflies.SetActive(false);
+    }
+}
