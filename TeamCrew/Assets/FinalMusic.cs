dcf892a3d9b499d81ca0d92663fb5bb4d5d67356
Emil Elthammar <emilelthammar@gmail.com>
2015-05-21 20:39:20 +0200

    Intense music

---
+++ TeamCrew/Assets/FinalMusic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalMusic : MonoBehaviour
+{
+    private AudioSource finalsound;
+    private Camera cam;
+	void Start () {
+        finalsound = transform.GetComponent<AudioSource>();
+        cam = Camera.main;
+	}
+
+    void Update()
+    {
+        if (cam.transform.position.y > -40)
+        {
+            PlayFinalMusic();
+        }
+    }
+	public void PlayFinalMusic()
+    {
+        if(!finalsound.isPlaying) finalsound.Play();
+    }
+}
