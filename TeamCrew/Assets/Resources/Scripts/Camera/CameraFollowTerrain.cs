719b0bb42dce80f7b506dd52a2daaa1ba8274975
SemihParlayan <semih.parlayan@hotmail.com>
2015-09-02 11:25:06 +0200

    Added a new resources folder, this may break YOUR LIFE!

---
+++ TeamCrew/Assets/Resources/Scripts/Camera/CameraFollowTerrain.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowTerrain : MonoBehaviour 
+{
+    public LayerMask terrainMask;
+    public float movementSpeed = 0.5f;
+    private Camera cam;
+
+	void Start ()
+    {
+        cam = Camera.main;
+	}
+
+	void Update () 
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x - 20, transform.position.y), Vector2.right, 200, terrainMask);
+        float targetX = transform.position.x;
+        if (hit)
+        {
+            Block b = hit.transform.GetComponent<Block>();
+            if (b)
+            {
+                if (b.difficulty == BlockDifficulty.Tutorial)
+                {
+                    targetX = hit.transform.position.x;
+                }
+                else
+                {
+                    //Set target X position to blocks bottomleft corner + 700pixels to center it
+                    targetX = b.GetStartCenterPosition.x;
+                }
+            }
+            else
+            {
+                targetX = hit.transform.position.x;
+            }
+        }
+
+        Vector3 targetPos = cam.transform.position;
+        targetPos.x = targetX;
+        cam.transform.position = Vector3.Lerp(cam.transform.position, targetPos, Time.deltaTime * movementSpeed);
+	}
+}
---
+++ TeamCrew/Assets/Resources/Scripts/LevelGeneration/Block.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BlockEnding 
+{ 
+    Thin,
+    Thick,
+    None
+}
+public enum BlockDifficulty
+{
+    Easy,
+    Medium,
+    Hard,
+    Top,
+    Tutorial
+}
+public class Block : MonoBehaviour 
+{
+    public static float ThinWidth = 12.26f;
+    public static float ThickWidth = 18.26f;
+
+    public BlockEnding end;
+    public int pixelsFromLeftEnd;
+
+    public BlockEnding start;
+    public int pixelsFromLeftStart;
+
+    public BlockDifficulty difficulty;
+
+    [HideInInspector]
+    public int blockIndex;
+
+    public Vector3 GetEndPosition
+    {
+        get
+        {
+            Vector3 pos = transform.position;
+            pos.x -= size.x / 2;
+            pos.x += pixelsFromLeftEnd / 100.0f;
+            pos.y += size.y / 2;
+            return pos;
+        }
+    }
+    public Vector3 GetStartPosition
+    {
+        get
+        {
+            Vector3 pos = transform.position;
+            pos.x -= size.x / 2;
+            pos.x += pixelsFromLeftStart / 100.0f;
+            pos.y -= size.y / 2;
+            return pos;
+        }
+    }
+    public Vector3 GetStartCenterPosition
+    {
+        get
+        {
+            Vector3 pos = GetStartPosition;
+
+            float pixelWidth = 913;
+
+            if (start == BlockEnding.Thin)
+                pixelWidth = 613;
+
+            pos.x += pixelWidth / 100.0f;
+            return pos;
+        }
+    }
+
+    public Vector3 size
+    {
+        get
+        {
+            return GetComponent<SpriteRenderer>().sprite.bounds.size;
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(GetEndPosition, new Vector3(0.1f, 0.1f));
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(GetStartPosition, new Vector3(0.1f, 0.1f));
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawCube(GetStartCenterPosition, new Vector3(0.2f, 0.2f));
+    }
+}
+
