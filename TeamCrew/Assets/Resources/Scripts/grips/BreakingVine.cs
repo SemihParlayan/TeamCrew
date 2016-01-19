e548e25f1c9e4bdc37031f389e84f3a3ae946fb3
SemihParlayan <semih.parlayan@hotmail.com>
2016-01-19 12:47:29 +0100

    Breaking vine and design frog

---
+++ TeamCrew/Assets/Resources/Scripts/grips/BreakingVine.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakingVine : MonoBehaviour
+{
+    public Grip[] snappingGrips;
+    private HingeJoint2D breakJoint;
+    private bool snapped = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        breakJoint = GetComponent<HingeJoint2D>();
+        if (breakJoint == null)
+        {
+            Debug.LogError("BreakingVine can't find hingejoint on object: " + transform.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (snapped)
+            return;
+
+        foreach(Grip g in snappingGrips)
+        {
+            foreach(GripPoint p in g.gripPoints)
+            {
+                if (p.numberOfHands > 0)
+                {
+                    SnapVine();
+                    break;
+                }
+            }
+
+            if (snapped)
+                break;
+        }
+
+	}
+
+    private void SnapVine()
+    {
+        snapped = true;
+        breakJoint.enabled = false;
+    }
+}
