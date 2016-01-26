697117f15a4b94186a6e15c4312e4d43b7aef921
SemihParlayan <semih.parlayan@hotmail.com>
2016-01-26 14:12:21 +0100

    Wheel angle gizmos added

--- TeamCrew/Assets/Resources/Scripts/grips/partygrips/SpinningWheelAngle.cs
+++ TeamCrew/Assets/Resources/Scripts/grips/partygrips/SpinningWheelAngle.cs
@@ -11,6 +11,7 @@ public class SpinningWheelAngle : SpinningWheel
     [Range(0.1f, 30f)]
     [Tooltip("How fast does the wheel change direction")]
     public float switchVelocity = 1f;
+    public float angleLimit;
 
     //privates
     private Vector2 dirOne;
@@ -19,15 +20,11 @@ public class SpinningWheelAngle : SpinningWheel
     private int dirModifier;
     private bool startDirectionFound;
     private bool decreaseSpeed;
-    private float angleLimit;
     private float startSpeed;
 
-    protected override void BaseStart()
+    void Awake()
     {
-        base.BaseStart();
-
         dirModifier = 1;
-        angleLimit = 3f;
         startSpeed = spinSpeed;
 
         if (greenAngle < 0)
@@ -36,6 +33,12 @@ public class SpinningWheelAngle : SpinningWheel
             redAngle = 360 + redAngle;
 
         targetDir = Vector2.zero;
+    }
+    protected override void BaseStart()
+    {
+        base.BaseStart();
+
+        
     }
     protected override void BaseUpdate()
     {
@@ -120,10 +123,20 @@ public class SpinningWheelAngle : SpinningWheel
     }
     void OnDrawGizmos()
     {
+        Vector2 ex1 = Vector2.zero;
+        Vector2 ex2 = Vector2.zero;
+        Vector2 ex3 = Vector2.zero;
+        Vector2 ex4 = Vector2.zero;
         if (!Application.isPlaying)
         {
             dirOne = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-redAngle + 90)), Mathf.Sin(Mathf.Deg2Rad * (-redAngle + 90)));
             dirTwo = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-greenAngle + 90)), Mathf.Sin(Mathf.Deg2Rad * (-greenAngle + 90)));
+
+            ex1 = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-redAngle + 90 + angleLimit)), Mathf.Sin(Mathf.Deg2Rad * (-redAngle + 90 + angleLimit)));
+            ex2 = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-redAngle + 90 - angleLimit)), Mathf.Sin(Mathf.Deg2Rad * (-redAngle + 90 - angleLimit)));
+
+            ex3 = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-greenAngle + 90 + angleLimit)), Mathf.Sin(Mathf.Deg2Rad * (-greenAngle + 90 + angleLimit)));
+            ex4 = new Vector2(Mathf.Cos(Mathf.Deg2Rad * (-greenAngle + 90 - angleLimit)), Mathf.Sin(Mathf.Deg2Rad * (-greenAngle + 90 - angleLimit)));
         }
 
         float rayLength = 4f;
@@ -136,10 +149,18 @@ public class SpinningWheelAngle : SpinningWheel
 
         //Draw maxLimit
         Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, ex1 * rayLength);
+        Gizmos.DrawRay(transform.position, ex2 * rayLength);
+
+        Gizmos.color = Color.red;
         Gizmos.DrawRay(transform.position, dirOne * rayLength);
         Gizmos.DrawSphere(transform.position + new Vector3(dirOne.x, dirOne.y) * rayLength, radius);
 
         //Draw minLimit
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, ex3 * rayLength);
+        Gizmos.DrawRay(transform.position, ex4 * rayLength);
+
         Gizmos.color = Color.green;
         Gizmos.DrawRay(transform.position, dirTwo * rayLength);
         Gizmos.DrawSphere(transform.position + new Vector3(dirTwo.x, dirTwo.y) * rayLength, radius);
