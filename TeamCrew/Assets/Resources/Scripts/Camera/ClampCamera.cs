12eabff19a17f037651d83bf9eca7d15df080883
SemihParlayan <semih.parlayan@hotmail.com>
2016-01-30 14:57:09 +0100

    Ny complete leve

---
+++ TeamCrew/Assets/Resources/Scripts/Camera/ClampCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClampCamera : MonoBehaviour 
+{
+    private Transform cameraTransform;
+
+    void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (cameraTransform.position.y + Camera.main.orthographicSize / 2 <= -44)
+        {
+            cameraTransform.position = new Vector3(cameraTransform.position.x, -44, cameraTransform.position.z);
+        }
+    }
+}
