6d3d3d23b7b184d9f8e7ac76e82433ce8b6204e2
SemihParlayan <semih.parlayan@hotmail.com>
2016-02-20 12:20:59 +0100

    VineBuilder

---
+++ TeamCrew/Assets/Resources/Scripts/Testing/VineBuilder/VineBuilder_Object.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VineBuilder_Object : MonoBehaviour 
+{
+    public Transform bottomPoint;
+
+    void OnDrawGizmos()
+    {
+        if (bottomPoint)
+        {
+            Gizmos.color = Color.white;
+            Gizmos.DrawWireSphere(bottomPoint.position, 0.01f);
+        }
+    }
+}
---
+++ TeamCrew/Assets/Resources/Scripts/Testing/VineBuilder/VineBuilder_Object_Editor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(VineBuilder_Object))]
+public class VineBuilder_Object_Editor : Editor 
+{
+    private bool settingBottomPoint;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        VineBuilder_Object obj = (VineBuilder_Object)target;
+
+        if (!settingBottomPoint)
+        {
+            if (GUILayout.Button("Set bottom point"))
+            {
+                settingBottomPoint = true;
+            }
+        }
+
+        if (settingBottomPoint)
+        {
+            if (GUILayout.Button("Stop"))
+            {
+                settingBottomPoint = false;
+            }
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+        VineBuilder_Object obj = (VineBuilder_Object)target;
+
+        Ray r = HandleUtility.GUIPointToWorldRay(e.mousePosition);
+        Vector3 mousePos = new Vector3(r.origin.x, r.origin.y);
+
+        switch (e.type)
+        {
+            case EventType.mouseDown:
+                {
+                    if (e.modifiers == EventModifiers.Alt)
+                        break;
+
+                    if (e.button == 0)
+                    {
+                        int controlId = GUIUtility.GetControlID(FocusType.Passive);
+                        GUIUtility.hotControl = controlId;
+                        Event.current.Use();
+
+                        if (settingBottomPoint)
+                        {
+                            settingBottomPoint = false;
+                            if (obj.bottomPoint == null)
+                            {
+                                GameObject o = new GameObject("bottomPoint");
+                                obj.bottomPoint = o.transform;
+                                obj.bottomPoint.parent = obj.transform;
+                            }
+                            obj.bottomPoint.position = mousePos;
+                        }
+                    }
+                    else
+                    {
+                        if (GUIUtility.hotControl != 0)
+                            GUIUtility.hotControl = 0;
+                    }
+                    break;
+                }
+        }
+
+        SceneView.RepaintAll();
+    }
+}
