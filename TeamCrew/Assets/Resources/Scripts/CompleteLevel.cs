b61063a0e5df398d0fc2ac630c83e1263cb02724
SemihParlayan <semih.parlayan@hotmail.com>
2016-02-22 18:46:03 +0100

    Johans music manager tillagd, inte korrekt inställd

---
+++ TeamCrew/Assets/Resources/Scripts/CompleteLevel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteLevel : MonoBehaviour 
+{
+    public Transform completeLevelPrefab = null;
+
+    void Start()
+    {
+        SetLevel();
+    }
+    void OnDrawGizmos()
+    {
+        SetLevel();
+    }
+
+    void SetLevel()
+    {
+        if (completeLevelPrefab == null)
+        {
+            completeLevelPrefab = Resources.Load("Prefabs/Complete level", typeof(Transform)) as Transform;
+        }
+
+        if (transform.position != completeLevelPrefab.position)
+            transform.position = completeLevelPrefab.position;
+    }
+}
