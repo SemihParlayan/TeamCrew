ef1e65239bcec434d8716180f24ef35f28e49d81
Sebastian Larsson <Sebastian.Larsson.3738@student.uu.se>
2016-03-03 11:03:38 +0100

    Added base animations for modifier icons :)

---
+++ TeamCrew/Assets/Resources/Scripts/Menu 2.0/Screens/ModeSelection/ModifierAnimatorCaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifierAnimatorCaller : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    public void IconSwitch()
+    {
+        anim.SetTrigger("IconSwitch");
+    }
+}
