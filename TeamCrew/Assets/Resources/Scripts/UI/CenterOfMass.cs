333509cf6a03743f4ef90577af4001f6cd75749b
SemihParlayan <semih.parlayan@hotmail.com>
2016-06-11 13:22:42 +0200

    Testade att ändra center of mass på alla lemmar

--- TeamCrew/Assets/Resources/Scripts/UI/CenterOfMass.cs
+++ TeamCrew/Assets/Resources/Scripts/UI/CenterOfMass.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class CenterOfMass : MonoBehaviour 
 {
+    public Vector2 centerOfMass;
     private Rigidbody2D body;
 
 	void Start () 
     {
         body = GetComponent<Rigidbody2D>();
-        body.centerOfMass = Vector2.zero;
+        //body.centerOfMass = Vector2.zero;
 	}
 
     void OnDrawGizmos()
@@ -16,8 +17,9 @@ public class CenterOfMass : MonoBehaviour
         Rigidbody2D body = GetComponent<Rigidbody2D>();
         if (body)
         {
+            body.centerOfMass = centerOfMass;
             Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(body.worldCenterOfMass, 0.1f);
+            Gizmos.DrawSphere(body.worldCenterOfMass, 0.075f);
         }   
     }
 }
